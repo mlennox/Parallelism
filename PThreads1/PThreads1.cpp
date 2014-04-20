ee9aaee4e4e94816c7527675e36b0134426cb4e1
mlennox <mark@webpusher.ie>
2014-04-20 22:23:55 +0100

    initialising project
    making sure I can setup a project that will build the phtread based code

---
+++ PThreads1/PThreads1.cpp
@@ -0,0 +1,18 @@
+// PThreads1.cpp : Defines the entry point for the console application.
+//
+
+// While in Visual Studio To ensure pthreads-win32 is referenced by the project:
+//		Click on project in solution explorer and hit Alt+Enter to see properties
+//		Click on Configuration Properties > Linker > Input and add pthreadsVC2.lib to the list of 'Additional Dependencies'
+// also need to add to C/C++ > General > Additional Include Directories
+// also need Linker > General > Additional Include Directories
+
+
+#include "stdafx.h"
+#include "pthread.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
